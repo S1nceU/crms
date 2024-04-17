82ccc92583b883dcafe117e603f63a8a62ebd657
S1nceU <jjkk900925@gmail.com>
2024-04-17 22:32:41 +0800

    Implement logout and fix set Cookie bug

--- module/user/delivery/http/http.go
+++ module/user/delivery/http/http.go
@@ -21,6 +21,7 @@ func NewUserHandler(e *gin.Engine, ser domain.UserService) {
 	{
 		api.POST("/userLogin", handler.Login)
 		api.POST("/userAuthentication", handler.Authentication)
+		api.POST("/userLogout", handler.Logout)
 	}
 }
 
@@ -52,7 +53,7 @@ func (u *UserHandler) Login(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{"msg": err.Error()})
 		return
 	}
-	c.SetCookie("token", token, int(_userSer.TokenExpireDuration.Seconds()), "/", "localhost:8090", false, true) // When the docker container is running, the domain should be changed to "localhost"
+	c.SetCookie("token", token, int(_userSer.TokenExpireDuration.Seconds()), "/", "", false, true) // When the docker container is running, the domain should be changed to "localhost"
 	c.JSON(http.StatusOK, gin.H{
 		"msg":   "Login successfully",
 		"token": token,
@@ -92,5 +93,41 @@ func (u *UserHandler) Authentication(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"msg":      "Authentication successfully",
 		"username": username,
+	})
+}
+
+// Logout @Summary Logout
+// @Description Logout
+// @Tags User
+// @Accept json
+// @Produce application/json
+// @Param UserTokenRequest body model.UserTokenRequest true "User JWT Token"
+// @Success 200 {object} string
+// @Router /userLogout [post]
+func (u *UserHandler) Logout(c *gin.Context) {
+	request := model.UserTokenRequest{}
+	if err := c.ShouldBindJSON(&request); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if _, err := c.Cookie("token"); err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"msg": "Not logged in yet"})
+		return
+	}
+
+	_, err := u.ser.Authentication(request.Token)
+
+	if err != nil {
+		if strings.HasPrefix(err.Error(), "token is expired") {
+			c.JSON(http.StatusUnauthorized, gin.H{"msg": err.Error()})
+			return
+		}
+		c.JSON(http.StatusInternalServerError, gin.H{"msg": err.Error()})
+		return
+	}
+	c.SetCookie("token", "", -1, "/", "", false, true) // When the docker container is running, the domain should be changed to "localhost"
+	c.JSON(http.StatusOK, gin.H{
+		"msg": "Logout successfully",
 	})
 }
